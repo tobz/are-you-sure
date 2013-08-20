4245e9f8b32eca6932237229bb6eadb7159f13e4
tobz <tobias.lawrence@gmail.com>
2013-08-20 13:56:07 +0000

    Changed the values accepted for proceeding to include short and long case of 'yes' and 'no' along with lowercase and capitalized versions.  Added the ability to respond negatively to dump out of the command.

--- main.go
+++ main.go
@@ -10,55 +10,61 @@ func init() {
 }
 
 func main() {
-    var err error
-    const AYS_PROCEED string = "Are you sure you want to run this?  Type 'yes' to proceed: "
+	var err error
+	const AYS_PROCEED string = "Are you sure you want to run this? [Y/n]: "
 
-    // Pull all the command arguments off the stack and reassemble them into the final command we're going to run.
-    commandToRun := strings.TrimSpace(strings.Join(os.Args[1:], " "))
-    if len(commandToRun) == 0 {
-        fmt.Println("[ays] You must specify a command to run!")
-        os.Exit(1)
-    }
+	// Pull all the command arguments off the stack and reassemble them into the final command we're going to run.
+	commandToRun := strings.TrimSpace(strings.Join(os.Args[1:], " "))
+	if len(commandToRun) == 0 {
+		fmt.Println("[ays] You must specify a command to run!")
+		os.Exit(1)
+	}
 
-    // We have our command, so spit it back out to the user and ask them to confirm running it.
-    fmt.Printf("[ays] Command entered: `%s`\n", commandToRun)
-    fmt.Print(AYS_PROCEED)
+	// We have our command, so spit it back out to the user and ask them to confirm running it.
+	fmt.Printf("[ays] Command entered: `%s`\n", commandToRun)
+	fmt.Print(AYS_PROCEED)
 
-    bufferedStdin := bufio.NewReader(os.Stdin)
-    for {
-        // Get their answer and check it.
-        answer, err := bufferedStdin.ReadString('\n')
-        if err != nil {
-            fmt.Printf("[ays] Caught an error while reading user input: %s", err.Error())
-            os.Exit(1)
-        }
+	bufferedStdin := bufio.NewReader(os.Stdin)
+	for {
+		// Get their answer and check it.
+		answer, err := bufferedStdin.ReadString('\n')
+		if err != nil {
+			fmt.Printf("[ays] Caught an error while reading user input: %s", err.Error())
+			os.Exit(1)
+		}
 
-        if strings.TrimSpace(answer) != "yes" {
-            fmt.Print(AYS_PROCEED)
-            continue
-        }
+		// Figure out what the user wants us to do.
+		switch strings.TrimSpace(answer) {
+		case "y", "Y", "yes", "Yes":
+			break
+		case "n", "N", "no", "No":
+			os.Exit(1)
+		default:
+			fmt.Print(AYS_PROCEED)
+			continue
+		}
 
-        // They answered yes, so let's go run the command.
-        break
-    }
+		// They answered yes, so let's go run the command.
+		break
+	}
 
-    // Now run the command!
-    fmt.Printf("[ays] Running command `%s`...\n", commandToRun)
+	// Now run the command!
+	fmt.Printf("[ays] Running command `%s`...\n", commandToRun)
 
-    commandPieces := strings.Split(commandToRun, " ")
-    if len(commandPieces) == 0 {
-        fmt.Printf("[ays] Couldn't get the pieces of the command!  Input string: %s\n", commandToRun)
-        os.Exit(1)
-    }
+	commandPieces := strings.Split(commandToRun, " ")
+	if len(commandPieces) == 0 {
+		fmt.Printf("[ays] Couldn't get the pieces of the command!  Input string: %s\n", commandToRun)
+		os.Exit(1)
+	}
 
-    command := exec.Command(commandPieces[0], commandPieces[1:]...)
-    command.Stdin = os.Stdin
-    command.Stdout = os.Stdout
-    command.Stderr = os.Stderr
+	command := exec.Command(commandPieces[0], commandPieces[1:]...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
 
-    err = command.Run()
-    if err != nil {
-        fmt.Printf("[ays] Caught an error while running '%s': %s", commandToRun, err.Error())
-        os.Exit(1)
-    }
+	err = command.Run()
+	if err != nil {
+		fmt.Printf("[ays] Caught an error while running '%s': %s", commandToRun, err.Error())
+		os.Exit(1)
+	}
 }
